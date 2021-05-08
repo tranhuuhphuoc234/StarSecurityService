e44710c48f918b21fc876f3d9168fac3d8770a5c
a <a>
2021-05-08 09:11:21 +0700

    template

---
+++ WebAPIAdmin/Models/role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIAdmin.Models
+{
+    [Table("role")]
+    public class role
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    }
+}
